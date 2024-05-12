d7d56a3e101aabe8156e30ecab63020b3ec37070
Luancss <ventobo123@gmail.com>
2024-05-12 18:35:41 -0300

    Authentication completed

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>
-      This is autenticated
-    </p>
+    <UserButton afterSignOutUrl="/" />
   )
 }
