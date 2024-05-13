3799f6b83ea4628d5ed89d40732cda6e1477984f
Luancss <ventobo123@gmail.com>
2024-05-13 01:35:40 -0300

    Hono.js setup

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -24,7 +24,6 @@ export default function Page() {
         </div>
       </div>
       <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
-
         <Image src="/logo.svg" height={100} width={100} alt="Logo" />
       </div>
     </div>
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,17 +1,16 @@
-import { z } from "zod"
 import { Hono } from "hono"
 import { handle } from "hono/vercel"
-import { zValidator } from "@hono/zod-validator";
+
+import authors from "./authors"
+import books from "./books"
 
 export const runtime = "edge";
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
-  });
-});
+app.route("/authors", authors);
+app.route("/books", books);
+
 
 export const GET = handle(app)
 export const POST = handle(app)
