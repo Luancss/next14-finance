11844d10728ae04a664717cc4503231f1fba2bde
Luancss <ventobo123@gmail.com>
2024-05-14 00:29:53 -0300

    Create a header component

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-full flex-items-center justify-between mb-14">
+          <div className="flex items-center lg:gap-x-16">
+            
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
