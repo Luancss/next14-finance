764119be971d7f92c756257ff14c4f40fae5bce3
Luancss <ventobo123@gmail.com>
2024-05-14 00:36:51 -0300

    Header logo component

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,12 @@
+import { HeaderLogo } from "@/components/header-logo"
+
 export const Header = () => {
   return (
     <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
       <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex-items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16">
-            
+            <HeaderLogo/>
           </div>
         </div>
       </div>
