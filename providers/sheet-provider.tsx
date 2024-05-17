d5cff97c116ecea7db374c22796ceb0f03cb1197
Luancss <ventobo123@gmail.com>
2024-05-17 01:26:59 -0300

    new account sheet component

---
+++ providers/sheet-provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+
+export const SheetProvider = () => {
+  return (
+    <>
+      <NewAccountSheet/>
+    </>
+  )
+}
