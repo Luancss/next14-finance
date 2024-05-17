8c2e4239b095e31431a258ac6724f56df8f32898
Luancss <ventobo123@gmail.com>
2024-05-17 00:58:31 -0300

    Add form from shadcn-ui

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,27 +1,49 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { HTTPException } from "hono/http-exception";
+import { createId } from "@paralleldrive/cuid2";
 
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
+import { zValidator } from "@hono/zod-validator";
 
-const app = new Hono().get("/", clerkMiddleware(), async (c) => {
-  const auth = getAuth(c);
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
 
-  if (!auth?.userId) {
-    return c.json({ error: "Unauthorized" }, 401);
-  }
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
 
-  const data = await db
-    .select({
-      id: accounts.id,
-      name: accounts.name,
-    })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId))
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
 
-  return c.json({ data });
-});
+    return c.json({ data });
+  })
+  .post("/", clerkMiddleware(),
+  zValidator("json", insertAccountSchema.pick({
+    name: true,
+  })),
+  async (c) => {
+    const auth = getAuth(c);
+    const values = c.req.valid("json");
+
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    const [data] = await db.insert(accounts).values({
+      id: createId(),
+      userId: auth.userId,
+      ...values,
+    }).returning();
+
+    return c.json({ data });
+  });
 
 export default app;
