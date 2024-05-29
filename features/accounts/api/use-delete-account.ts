b8214b6db7b5dec31435477f7628f39eabec3f97
Luancss <ventobo123@gmail.com>
2024-05-29 01:42:00 -0300

    Delete account component

---
+++ features/accounts/api/use-delete-account.ts
@@ -0,0 +1,32 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$delete"]>;
+
+export const useDeleteAccount = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<
+    ResponseType,
+    Error
+  >({
+    mutationFn: async () => {
+      const response = await client.api.accounts[":id"]["$delete"]({ 
+        param: { id },
+       });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Account deleted");
+      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+    onError: () => {
+      toast.error("Failed to delete account");
+    },
+  });
+
+  return mutation;
+};
--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -1,4 +1,3 @@
-
 import {
   Sheet,
   SheetContent,
@@ -8,62 +7,67 @@ import {
 } from "@/components/ui/sheet";
 import { insertAccountSchema } from "@/db/schema";
 import { AccountForm } from "@/features/accounts/components/account-form";
-import {  } from "@/features/accounts/hooks/use-new-account";
+import {} from "@/features/accounts/hooks/use-new-account";
 import { z } from "zod";
-import { useCreateAccount } from "@/features/accounts/api/use-create-account";
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
 import { useGetAccount } from "../api/use-get-account";
 import { Loader2 } from "lucide-react";
+import { useEditAccount } from "@/features/accounts/api/use-edit-account";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
 
 const formSchema = insertAccountSchema.pick({
   name: true,
 });
 
-type FormValues = z.infer<typeof formSchema>
+type FormValues = z.infer<typeof formSchema>;
 export const EditAccountSheet = () => {
   const { isOpen, onClose, id } = useOpenAccount();
 
   const accountQuery = useGetAccount(id);
-  const mutation = useCreateAccount();
+  const editMutation = useEditAccount(id);
+  const deleteMutation = useDeleteAccount(id);
+
+  const isPending = editMutation.isPending || deleteMutation.isPending;
 
   const isLoading = accountQuery.isLoading;
 
   const onSubmit = (values: FormValues) => {
-    mutation.mutate(values, {
+    editMutation.mutate(values, {
       onSuccess: () => {
         onClose();
-      }
+      },
     });
-  }
+  };
 
-  const defaultValues = accountQuery.data ? {
-    name: accountQuery.data.name
-  } : {
-    name: "",
-  }
+  const defaultValues = accountQuery.data
+    ? {
+        name: accountQuery.data.name,
+      }
+    : {
+        name: "",
+      };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
         <SheetHeader>
           <SheetTitle>Edit account</SheetTitle>
-          <SheetDescription>
-            Edit an existing account
-          </SheetDescription>
+          <SheetDescription>Edit an existing account</SheetDescription>
         </SheetHeader>
         {isLoading ? (
           <div className="absolute inset-9 flex items-center justify-center">
-            <Loader2 className="size-4 text-muted-foreground animate-spin"/>
+            <Loader2 className="size-4 text-muted-foreground animate-spin" />
           </div>
         ) : (
-          <AccountForm 
-          id={id}
-          onSubmit={onSubmit}
-          disabled={mutation.isPending}
-          defaultValues={defaultValues}
-         />
+          <AccountForm
+            id={id}
+            onSubmit={onSubmit}
+            disabled={isPending}
+            defaultValues={defaultValues}
+            onDelete={() => deleteMutation.mutate()}
+          />
         )}
       </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
