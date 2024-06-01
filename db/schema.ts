087a6e64f93ec610bf31738f9b51b40660fd42c0
Luancss <ventobo123@gmail.com>
2024-06-01 15:34:27 -0300

    Add transaction schema with relations and date coercion

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,7 @@
 import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
+import { relations } from "drizzle-orm";
+import { z } from "zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -7,6 +9,10 @@ export const accounts = pgTable("accounts", {
   name: text("name").notNull(),
   userId: text("user_Id").notNull(),
 });
+
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions),
+}));
 
 export const insertAccountSchema = createInsertSchema(accounts);
 
@@ -17,6 +23,10 @@ export const categories = pgTable("categories", {
   userId: text("user_Id").notNull(),
 });
 
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
 export const insertCategorySchema = createInsertSchema(categories);
 
 export const transactions = pgTable("transactions", {
@@ -24,5 +34,28 @@ export const transactions = pgTable("transactions", {
   amount: integer("amount").notNull(),
   payee: text("payee").notNull(),
   notes: text("notes"),
-  date: timestamp("date", {mode: "date"}).notNull(),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id")
+    .references(() => accounts.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+});
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  accounts: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  categories: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
 });
