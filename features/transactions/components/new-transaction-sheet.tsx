b0c44acc513013f4c82b66f1d70e3b6a5c8f1f66
Luancss <ventobo123@gmail.com>
2024-06-28 01:23:47 -0300

    Added category creation functionality

--- features/transactions/components/new-transaction-sheet.tsx
+++ features/transactions/components/new-transaction-sheet.tsx
@@ -11,6 +11,8 @@ import { AccountForm } from "@/features/accounts/components/account-form";
 import { useNewTransaction } from "../hooks/use-new-transaction";
 import { z } from "zod";
 import { useCreateTransaction } from "../api/use-create-transaction";
+import { useCreateCategory } from "@/features/categories/api/use-create-category";
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
 
 const formSchema = insertTransactionSchema.omit({
   id: true,
@@ -21,6 +23,19 @@ export const NewTransactionSheet = () => {
   const { isOpen, onClose } = useNewTransaction();
 
   const mutation = useCreateTransaction();
+
+  const categoryQuery = useGetCategories();
+  const categoryMutation = useCreateCategory();
+  const onCreateCategory = (name: string) => {
+    categoryMutation.mutate({
+      name
+    });
+  }
+
+  const categoryOptions = (categoryQuery.data ?? []).map((category) => ({
+    label: category.name,
+    value: category.id
+  }))
 
   const onSubmit = (values: FormValues) => {
     mutation.mutate(values, {
