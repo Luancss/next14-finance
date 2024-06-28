f3d1bb5ae66b0e6f1e559d68d49849bff2773f07
Luancss <ventobo123@gmail.com>
2024-06-28 01:52:09 -0300

    Added TransactionForm component for managing account transactions

--- features/transactions/components/new-transaction-sheet.tsx
+++ features/transactions/components/new-transaction-sheet.tsx
@@ -1,4 +1,17 @@
+import { z } from "zod";
+import { Loader2 } from "lucide-react";
 
+import { TransactionForm } from "@/features/transactions/components/transaction-form";
+import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
+import { useCreateTransaction } from "@/features/transactions/api/use-create-transaction";
+
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
+import { useCreateCategory } from "@/features/categories/api/use-create-category";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+
+import { insertTransactionSchema } from "@/db/schema";
 import {
   Sheet,
   SheetContent,
@@ -6,56 +19,84 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import { insertTransactionSchema } from "@/db/schema";
-import { AccountForm } from "@/features/accounts/components/account-form";
-import { useNewTransaction } from "../hooks/use-new-transaction";
-import { z } from "zod";
-import { useCreateTransaction } from "../api/use-create-transaction";
-import { useCreateCategory } from "@/features/categories/api/use-create-category";
-import { useGetCategories } from "@/features/categories/api/use-get-categories";
 
 const formSchema = insertTransactionSchema.omit({
   id: true,
 });
 
-type FormValues = z.infer<typeof formSchema>
+type FormValues = z.input<typeof formSchema>;
+
 export const NewTransactionSheet = () => {
   const { isOpen, onClose } = useNewTransaction();
 
-  const mutation = useCreateTransaction();
+  const createMutation = useCreateTransaction();
 
   const categoryQuery = useGetCategories();
   const categoryMutation = useCreateCategory();
-  const onCreateCategory = (name: string) => {
-    categoryMutation.mutate({
-      name
-    });
-  }
-
+  const onCreateCategory = (name: string) => categoryMutation.mutate({
+    name
+  });
   const categoryOptions = (categoryQuery.data ?? []).map((category) => ({
     label: category.name,
-    value: category.id
-  }))
+    value: category.id,
+  }));
+
+  const accountQuery = useGetAccounts();
+  const accountMutation = useCreateAccount();
+  const onCreateAccount = (name: string) => accountMutation.mutate({
+    name
+  });
+  const accountOptions = (accountQuery.data ?? []).map((account) => ({
+    label: account.name,
+    value: account.id,
+  }));
+
+  const isPending =
+    createMutation.isPending ||
+    categoryMutation.isPending ||
+    accountMutation.isPending;
+
+  const isLoading =
+    categoryQuery.isLoading ||
+    accountQuery.isLoading;
 
   const onSubmit = (values: FormValues) => {
-    mutation.mutate(values, {
+    createMutation.mutate(values, {
       onSuccess: () => {
         onClose();
-      }
+      },
     });
-  }
+  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
         <SheetHeader>
-          <SheetTitle>New Account</SheetTitle>
+          <SheetTitle>
+            New Transaction
+          </SheetTitle>
           <SheetDescription>
             Add a new transaction
           </SheetDescription>
         </SheetHeader>
-        <p>TODO: Transaction Form</p>
+        {isLoading
+          ? (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <Loader2 className="size-4 text-muted-foreground animate-spin" />
+            </div>
+          )
+          : (
+            <TransactionForm
+              onSubmit={onSubmit}
+              disabled={isPending}
+              categoryOptions={categoryOptions}
+              onCreateCategory={onCreateCategory}
+              accountOptions={accountOptions}
+              onCreateAccount={onCreateAccount}
+            />
+          )
+        }
       </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
--- features/transactions/components/transaction-form.tsx
+++ features/transactions/components/transaction-form.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { insertAccountSchema } from "@/db/schema"
+import { insertAccountSchema, insertTransactionSchema } from "@/db/schema"
 import {
   Form,
   FormControl,
@@ -15,26 +15,43 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 
-const formSchema = insertAccountSchema.pick({
-   name: true,
+const formSchema = z.object({
+  data: z.coerce.date(),
+  accountId: z.string(),
+  categoryId: z.string().nullable().optional(),
+  payee: z.string(),
+  amount: z.string(),
+  notes: z.string().nullable().optional()
+})
+
+const apiSchema = insertTransactionSchema.omit({
+  id: true,
 });
 
-type FormValues = z.infer<typeof formSchema>
+type FormValues = z.infer<typeof formSchema>;
+type ApiFormValues = z.input<typeof apiSchema>;
 
 type Props = {
   id?: string;
   defaultValues?: FormValues;
-  onSubmit: (data: FormValues) => void;
+  onSubmit: (data: ApiFormValues) => void;
   onDelete?: () => void;
   disabled?: boolean;
+  accountOptions: {label: string; value: string;} [];
+  onCreateAccount: (name: string) => void;
+  onCreateCategory: (data: string) => void;
 };
 
-export const AccountForm = ({
+export const TransactionForm = ({
   id,
   defaultValues,
   onSubmit,
   onDelete,
   disabled,
+  accountOptions,
+  categoryOptions,
+  onCreateAccount,
+  onCreateCategory
 }: Props) => {
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
