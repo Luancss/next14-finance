8bc4f77c4753e5c54ab9ea0047568a27dc0ab384
Luancss <ventobo123@gmail.com>
2024-07-07 22:05:36 -0300

    implement dynamic column selection in ImportTable

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -2,6 +2,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { UploadButton } from "./upload-button";
+import { useState } from "react";
+import { ImportTable } from "./import-table";
 
 const dateFormat = "yyyy-MM-dd HH:mm:ss";
 const outputFormat = "yyyy-MM-dd";
@@ -19,6 +21,35 @@ type Props = {
 };
 
 export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
+  const [selectedColumns, setSelectedColumns] = useState<SelectedColumnsState>(
+    {}
+  );
+
+  const headers = data[0];
+  const body = data.slice(1);
+
+  const onTableHeadSelectChange = (
+    columnIndex: number,
+    value: string | null
+  ) => {
+    setSelectedColumns((prev) => {
+      const newSelectedColumns = { ...prev };
+
+      for (const key in newSelectedColumns) {
+        if (newSelectedColumns[key] === value) {
+          newSelectedColumns[key] = null;
+        }
+      }
+
+      if (value === "skip") {
+        value = null;
+      }
+
+      newSelectedColumns[`column_${columnIndex}`] = value;
+      return newSelectedColumns;
+    });
+  };
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -32,7 +63,14 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
             </Button>
           </div>
         </CardHeader>
-        <CardContent>Hello</CardContent>
+        <CardContent>
+          <ImportTable
+            headers={headers}
+            body={body}
+            selectedColumns={selectedColumns}
+            onTableHeadSelectChange={onTableHeadSelectChange}
+          />
+        </CardContent>
       </Card>
     </div>
   );
