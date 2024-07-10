be75b857f81ec3313a3a9d76184babd31816e0fa
Luancss <ventobo123@gmail.com>
2024-07-10 19:31:32 -0300

    Added financial data comparison and percentage change calculation

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -1,5 +1,10 @@
+import { db } from "@/db/drizzle";
+import { accounts, transactions } from "@/db/schema";
+import { calculatePercentageChange } from "@/lib/utils";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
+import { differenceInDays, parse, subDays } from "date-fns";
+import { and, eq, gte, lte, sql, sum } from "drizzle-orm";
 import { Hono } from "hono";
 import { z } from "zod";
 
@@ -21,6 +26,79 @@ const app = new Hono().get(
     if (!auth?.userId) {
       return c.json({ error: "Unauthorized" }, 401);
     }
+
+    const defaultTo = new Date();
+    const defaultFrom = subDays(defaultTo, 30);
+
+    const startDate = from
+      ? parse(from, "yyyy-MM-dd", new Date())
+      : defaultFrom;
+    const endDate = to ? parse(to, "yyyy-MM-dd", new Date()) : defaultTo;
+
+    const periodLength = differenceInDays(endDate, startDate) + 1;
+    const lastPeriodStart = subDays(startDate, periodLength);
+    const lastPeriodEnd = subDays(endDate, periodLength);
+
+    async function fetchFinancialData(
+      userId: string,
+      startDate: Date,
+      endDate: Date
+    ) {
+      return await db
+        .select({
+          income:
+            sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+              Number
+            ),
+          expenses:
+            sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+              Number
+            ),
+          remaining: sum(transactions.amount).mapWith(Number),
+        })
+        .from(transactions)
+        .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+        .where(
+          and(
+            accountId ? eq(transactions.accountId, accountId) : undefined,
+            eq(accounts.userId, userId),
+            gte(transactions.date, startDate),
+            lte(transactions.date, endDate)
+          )
+        );
+    }
+
+    const [currentPeriod] = await fetchFinancialData(
+      auth.userId,
+      startDate,
+      endDate,
+    );
+    const [lastPeriod] = await fetchFinancialData(
+      auth.userId,
+      lastPeriodStart,
+      lastPeriodEnd,
+    );
+
+    const incomeChange = calculatePercentageChange(
+      currentPeriod.income,
+      lastPeriod.income,
+    );
+    const expensesChange = calculatePercentageChange(
+      currentPeriod.expenses,
+      lastPeriod.expenses,
+    );
+    const remainingChange = calculatePercentageChange(
+      currentPeriod.remaining,
+      lastPeriod.remaining,
+    );
+
+    return c.json({
+      currentPeriod,
+      lastPeriod,
+      incomeChange,
+      expensesChange,
+      remainingChange,
+    })
   }
 );
 
