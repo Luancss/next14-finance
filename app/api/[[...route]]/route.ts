a12584b59130411473df8e998185c9412fdce8e9
Luancss <ventobo123@gmail.com>
2024-07-10 19:10:14 -0300

    Added authentication middleware and query validation structure

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,18 +1,20 @@
-import { Hono } from "hono"
+import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import accounts from "./accounts";
 import categories from "./categories";
 import transactions from "./transactions";
+import summary from "./summary";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 const routes = app
+  .route("/summary", summary)
   .route("/accounts", accounts)
   .route("/categories", categories)
-  .route("/transactions", transactions)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
