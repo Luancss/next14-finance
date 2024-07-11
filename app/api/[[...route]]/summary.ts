fcf54af7b6a12a27ca2954c67dfb84ed1435fa09
Luancss <ventobo123@gmail.com>
2024-07-11 01:37:16 -0300

    Add category expense aggregation query

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -1,10 +1,10 @@
 import { db } from "@/db/drizzle";
-import { accounts, transactions } from "@/db/schema";
+import { accounts, categories, transactions } from "@/db/schema";
 import { calculatePercentageChange } from "@/lib/utils";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 import { differenceInDays, parse, subDays } from "date-fns";
-import { and, eq, gte, lte, sql, sum } from "drizzle-orm";
+import { and, desc, eq, gte, lt, lte, sql, sum } from "drizzle-orm";
 import { Hono } from "hono";
 import { z } from "zod";
 
@@ -91,6 +91,40 @@ const app = new Hono().get(
       currentPeriod.remaining,
       lastPeriod.remaining,
     );
+
+    const category = await db
+        .select({
+          name: categories.name,
+          value: sql`SUM(ABS(${transactions.amount}))`.mapWith(Number),
+        })
+        .from(transactions)
+        .innerJoin(
+          accounts,
+          eq(
+            transactions.accountId,
+            accounts.id,
+          ),
+        )
+        .innerJoin(
+          categories,
+          eq(
+            transactions.categoryId,
+            categories.id,
+          )
+        )
+        .where(
+          and(
+            accountId ? eq(transactions.accountId, accountId) : undefined,
+            eq(accounts.userId, auth.userId),
+            lt(transactions.amount, 0),
+            gte(transactions.date, startDate),
+            lte(transactions.date, endDate),
+          )
+        )
+        .groupBy(categories.name)
+        .orderBy(desc(
+          sql`SUM(ABS(${transactions.amount}))`
+        ));
 
     return c.json({
       currentPeriod,
